f01b3337a30fcedce97705f0a6b2095361386398
tarikkarakas <tarik.karakas@pwc.com>
2021-05-19 15:05:29 +0300

    fix

--- app.py
+++ app.py
@@ -1,31 +1,84 @@
 import functions as func # import functions.py file
 import streamlit as st #streamlit
+from streamlit import caching
 import datetime 
 from datetime import timedelta 
 import pandas as pd
-
 import plotly.express as px
 import plotly.graph_objects as go
 
-start_date=st.sidebar.date_input(label="Start Date", value=datetime.date.today()-datetime.timedelta(days=1))
-end_date=st.sidebar.date_input("End Date", value=datetime.date.today())
 
-#PTF
-mcp_data = func.mcp(startDate=str(start_date),endDate=str(end_date))
-mcp_data.head()
-#Consumption
-consumption_data = func.consumption_realtime(startDate=str(start_date),endDate=str(end_date))
-consumption_data.head()
+st.set_page_config(page_title ="Forecasting Tool")
+
+tabs = ["Data Visualization","Forecasting","About"]
+page = st.sidebar.radio("Tabs",tabs)
+
+#@st.cache(persist=False,allow_output_mutation=True,suppress_st_warning=True,show_spinner= True)
+
+
+if page=="Data Visualization":
+    start_date=st.sidebar.date_input(label="Start Date", value=datetime.date.today()-datetime.timedelta(days=10))
+    end_date=st.sidebar.date_input(label="End Date", value=datetime.date.today())
+    st.markdown("<h1 style='text-align: center; color: black;'>Forecasting Tool-Visualization Tab</h1>", unsafe_allow_html=True)
+    st.markdown("""This tool aims to generate time series forecast for Turkey's electric power industry. Data comes from
+     [EPIAS Transparency Platform](https://seffaflik.epias.com.tr/transparency/index.xhtml) and updated hourly.""")
+
+    st.markdown("""We use several algorithm for forecasting. The documentation of the algorithms are:
+      **[Prophet](https://facebook.github.io/prophet/)**.""")
+    if end_date > start_date:
+        #Import Consumption data from the source.
+        consumption_data = func.consumption_realtime(startDate=str(start_date),endDate=str(end_date))
+        cons_describe=pd.DataFrame(consumption_data.describe()).reset_index().rename(index=str,columns={"index":"Statistic"})
+        #Consumption-Descriptive Statistics
+        st.markdown("<h3 style='text-align: center; color: black;'>Consumption-Descriptive Statistics</h3>", unsafe_allow_html=True)
+        st.table(cons_describe)
+        #Consumption-Hourly Graph
+        fig_cons = go.Figure()
+        fig_cons.add_trace(go.Scatter(x=consumption_data.Date, y=consumption_data.Consumption, mode='lines',name='Consumption (MWh)'))
+        fig_cons.update_layout(xaxis_title='Date',yaxis_title='Consumption (MWh)',plot_bgcolor='rgba(0,0,0,0)')
+        st.markdown("<h3 style='text-align: center; color: black;'>Hourly Consumption (MWh)</h3>", unsafe_allow_html=True)
+        st.plotly_chart(fig_cons)
+
+        #Import MCP data from the source.
+        mcp_data = func.mcp(startDate=str(start_date),endDate=str(end_date))
+        mcp_describe=pd.DataFrame(mcp_data.describe()).reset_index().rename(index=str,columns={"index":"Statistic"})
+        #MCP-Descriptive Statistics
+        st.markdown("<h3 style='text-align: center; color: black;'>Market Clearing Price-Descriptive Statistics</h3>", unsafe_allow_html=True)
+        st.table(mcp_describe)
+        #st.table(pd.DataFrame(mcp_data.describe()).reset_index().rename(index=str,columns={"index":"Statistic"}))
+        #MCP-Hourly Graph
+        st.markdown("<h3 style='text-align: center; color: black;'>Market Clearing Price-TL</h3>", unsafe_allow_html=True)
+        fig_mcp = go.Figure()
+        fig_mcp.add_trace(go.Scatter(x=mcp_data.Date, y=mcp_data.MCP_TL, mode='lines',name='MCP TL'))
+        fig_mcp.update_layout(xaxis_title='Date',yaxis_title='MCP',plot_bgcolor='rgba(0,0,0,0)')
+        st.plotly_chart(fig_mcp)
+
+    else:
+        st.warning('Start date cannot bigger than end date. Please provide the dates again.')
+
+
+
+if page=="About":
+    st.header("About")
+    st.write("")
+    st.write("Author:")
+    st.markdown(""" **[Tarik Karakas](https://tr.linkedin.com/in/karakastarik)**""")
+    st.markdown(""" **[Recep Alcep](https://tr.linkedin.com/in/recepalcep)**""")
+    st.markdown("""**[Source code](https://github.com/karakastarik/epias-transparency-forecasting-tool)**""")
+
+    st.write("Created on 10/05/2021")
+
+
+
 #Real time generation
-real_time_gen_data = func.real_time_gen(startDate=str(start_date),endDate=str(end_date))
-real_time_gen_data.head()
+#real_time_gen_data = func.real_time_gen(startDate=str(start_date),endDate=str(end_date))
+#real_time_gen_data.head()
 
-#Stremlit Side
-st.markdown("<h1 style='text-align: center; color: black;'>Forecasting Tool</h1>", unsafe_allow_html=True)
-cons_describe=pd.DataFrame(consumption_data.describe()).reset_index().rename(index=str,columns={"index":"Statistic"})
 
-st.markdown("<h3 style='text-align: center; color: black;'>Consumption-Descriptive Statistics</h3>", unsafe_allow_html=True)
-st.table(cons_describe)
+
+
+
+
 #st.table(pd.DataFrame(consumption_data.describe()).reset_index().rename(index=str,columns={"index":"Statistic"}))
 #cons_table = go.Figure(data=[go.Table(
 #    header=dict(values=list(cons_describe.columns),
@@ -37,21 +90,11 @@ st.table(cons_describe)
 #])
 
 #st.plotly_chart(cons_table)
-st.markdown("<h3 style='text-align: center; color: black;'>Hourly Consumption (MWh)</h3>", unsafe_allow_html=True)
+
 #cons_markdown = f"Average hourly consumption in the last **{consumption_data.Date.count()}** hours is: **{consumption_data.Consumption.mean():.2f}**."
 #st.markdown(cons_markdown)
-fig_cons = go.Figure()
-fig_cons.add_trace(go.Scatter(x=consumption_data.Date, y=consumption_data.Consumption, mode='lines',name='Consumption (MWh)'))
-fig_cons.update_layout(xaxis_title='Date',yaxis_title='Consumption (MWh)',plot_bgcolor='rgba(0,0,0,0)')
-st.plotly_chart(fig_cons)
 
-st.markdown("<h3 style='text-align: center; color: black;'>Market Clearing Price-Descriptive Statistics</h3>", unsafe_allow_html=True)
-st.table(pd.DataFrame(mcp_data.describe()).reset_index().rename(index=str,columns={"index":"Statistic"}))
-st.markdown("<h3 style='text-align: center; color: black;'>Market Clearing Price-TL</h3>", unsafe_allow_html=True)
-fig_mcp = go.Figure()
-fig_mcp.add_trace(go.Scatter(x=mcp_data.Date, y=mcp_data.MCP_TL, mode='lines',name='MCP TL'))
-fig_mcp.update_layout(xaxis_title='Date',yaxis_title='MCP',plot_bgcolor='rgba(0,0,0,0)')
-st.plotly_chart(fig_mcp)
+
 
 #fig = px.line(data, x=~Date,y = ~Consumption,name = "Consumption", type = 'scatter', mode = 'lines') 
 #fig = fig.add_trace(data,y = ~Generation , name = 'Generation', mode = 'lines') 
